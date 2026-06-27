89250126032ee85ecc0040ca08d1a4ef4510a96d
ghostware0x00 <souravbose361@gmail.com>
2026-06-28 00:19:35 +0530

    code update

--- omniRecon.py
+++ omniRecon.py
@@ -3,10 +3,25 @@ import argparse
 from colorama import Fore, Style, init # colorama to add color to text displayed using print() function
 init(autoreset=True)# resets color of the print() text each time 
 
-
+COMMON_SERVICES = {
+    21: "ftp",
+    22: "ssh",
+    23: "telnet",
+    25: "smtp",
+    53: "dns",
+    80: "http",
+    110: "pop3",
+    143: "imap",
+    443: "https",
+    445: "smb",
+    3306: "mysql",
+    3389: "rdp",
+    5432: "postgresql",
+    6379: "redis",
+}
 
 def display_scan_output(scan_results): # display scan_result output from the dictionary
-    print(f"{scan_results['port']}\t{scan_results['state']}\t{scan_results['service']}\t{scan_results['version']}")
+    print(f"{Style.BRIGHT}{Fore.LIGHTGREEN_EX}{scan_results['port']}\t{scan_results['state']}\t{scan_results['service']}\t{scan_results['version']}")
     
 
 def bannerGrab_ServerFirstArch(client): # services which do not require the client to send data first but server responds immediately upon connection
@@ -42,7 +57,10 @@ def fullTCPScan(TARGET, PORT):
             port = int(port) # convert string to int (since PORT is taken in command line argument)
             try:
                 scan_results = {"port":port, "state":"", "service":"", "version":""}
-                scan_results['service'] = socket.getservbyport(port, "tcp")
+                try:
+                    scan_results['service'] = socket.getservbyport(port, "tcp")
+                except OSError as e:
+                    scan_results['service'] = COMMON_SERVICES.get(port, 'unknown') # if common services list doesn't have that particular port service info then store unknown
                 client.settimeout(10)
                 client.connect((TARGET, port))
                 if port == 80: #HTTP
