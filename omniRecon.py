576c1e994bea9321bc7b9e51f66d6fe204ffd84f
ghostware0x00 <souravbose361@gmail.com>
2026-06-27 23:18:43 +0530

    code update

--- omniRecon.py
+++ omniRecon.py
@@ -4,15 +4,25 @@ from colorama import Fore, Style, init # colorama to add color to text displayed
 init(autoreset=True)# resets color of the print() text each time 
 
 
+def scan_results_template(port, state, service=None, version=None):
+    scan_results = {"port":"", "state":"", "service":"", "version":""}
+    scan_results['port'] = port
+    scan_results['state'] = state
+    scan_results['service'] = service
+    scan_results['version'] = version
+    print(f"{scan_results['port']}\t{scan_results['state']}\t{scan_results['service']}\t{scan_results['version']}")
+
+
 
 def bannerGrab_ServerFirstArch(client): # services which do not require the client to send data first but server responds immediately upon connection
     service_info = client.recv(4096)
     if service_info:
-        print(f"{Fore.CYAN}{service_info.decode()}")
+        print(f"{service_info.decode()}")
 
 
 
 def bannerGrab_HTTP(client, TARGET): # banner grabbing for http
+    scan_results = {"port":"80", "state":"open", "service":"", "version":""}
     request_payload = (
         f"GET / HTTP/1.1\r\n"
         f"Host: {TARGET}\r\n"
@@ -22,36 +32,38 @@ def bannerGrab_HTTP(client, TARGET): # banner grabbing for http
     client.sendall(request_payload.encode())# encoding to utf8 format
     response = client.recv(4096)
     if response:
-        print(f"{Fore.GREEN}{response.decode()}")
+        response = response.decode().split('\r\n')
+        print(f"{response}")
 
 
 
-def port_scanning_and_service_enumeration(TARGET, PORT):
+def fullTCPScan(TARGET, PORT):
     # the port_scanning should accept multiple ports
     # this can be implemented using nargs in argsparser
-    print(f"{Fore.GREEN}[*]Initiating OMNIRECON Port Scanning and Service Enumeration on {TARGET} on port {PORT}")
-    print(f"{Fore.GREEN}<---------------- SCAN RESULTS ---------------->")
-    try:        
-        for port in PORT:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:# creating a socket object # using the with keyword the socket object is closed automatically matter what
-                port = int(port) # convert string to int (since PORT is taken in command line argument)
-                try:
-                    client.settimeout(3)
-                    client.connect((TARGET, port))
-                    if port == 80: #HTTP
-                        bannerGrab_HTTP(client, TARGET) # function to handle http
-                    elif port in (22, 21, 25, 3306): # SSH FTP SMTP MySQL
-                        bannerGrab_ServerFirstArch(client) # handle server first architecture protocols
-                    #elif port 
-                except TimeoutError as t: # port might be open but firewall might filter that so it will drop packets (in that scenario we check so timeout) so that the socket doesn't wait indefinetly
-                    print(f"{Fore.GREEN}{port}\tFiltered")
-                except ConnectionRefusedError as c: # port might be closed so the server will send rst flag refusing connection (such scenario raises this exception)
-                    print(f"{Fore.GREEN}{PORT}\tClosed")
-                except OSError as e:
-                    print(f"{Fore.RED}Couldn't connect to target. Target Unreachable: {e}")
-                    break
-    except KeyboardInterrupt:
-        print(f"{Fore.RED}[x]closing omniRecon port scanning...")
+    print(f"{Style.BRIGHT}{Fore.GREEN}[*]Starting OMNIRECON Full TCP Port Scanning and Service Enumeration on {TARGET} on port {PORT}")
+    print(f"<---------------- SCAN RESULTS ---------------->")   
+    print(f"PORT\tSTATE\tSERVICE\tVERSION") 
+    for port in PORT:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:# creating a socket object # using the with keyword the socket object is closed automatically matter what
+            port = int(port) # convert string to int (since PORT is taken in command line argument)
+            try:
+                client.settimeout(10)
+                client.connect((TARGET, port))
+                scan_results = {"port":port, "state":"", "service":"", "version":""}
+                if port == 80: #HTTP
+                    bannerGrab_HTTP(client, TARGET) # function to handle http
+                elif port in (22, 21, 25, 3306): # SSH FTP SMTP MySQL
+                    bannerGrab_ServerFirstArch(client) # handle server first architecture protocols
+                #elif port 
+            except TimeoutError as t: # port might be open but firewall might filter that so it will drop packets (in that scenario we check so timeout) so that the socket doesn't wait indefinetly
+                scan_results['state'] = 'Filtered'
+            except ConnectionRefusedError as c: # port might be closed so the server will send rst flag refusing connection (such scenario raises this exception)
+                scan_results['state'] = 'Closed'
+            except OSError as e:
+                print(f"{Fore.RED}Couldn't connect to target. Target Unreachable: {e}")
+                break
+            except KeyboardInterrupt:
+                print(f"{Fore.RED}[x]closing omniRecon port scanning...")
 
 
 
@@ -59,14 +71,15 @@ def parseArgs():
     parser = argparse.ArgumentParser(description ="take omnirecon arguments") 
     parser.add_argument("-t", "--target", metavar="www.example.com", help="Provide domain name or IP address") # metavar = placeholder for the value which you need to give # help = description for the placeholder about what to input
     parser.add_argument("-p", "--port", nargs='*' ,metavar="80", help="Provide target port to connect with") # nargs allows multiple values for a single command line argument also nargs='*' allows 0 or more values to be given as command line value. nargs takes the values as list []
+    parser.add_argument("-sT", "--fullTCP", action="store_true", help="Perform Full TCP Scan (3 way Handshake)")
     return parser.parse_args()
 
 
 
 def get_args():
     args = parseArgs() # (args) stores the arguments suppied
-    if args.target and args.port:
-        port_scanning_and_service_enumeration(args.target, args.port)
+    if args.fullTCP and args.target and args.port:
+        fullTCPScan(args.target, args.port)
     else:
         print(f"{Style.BRIGHT}{Fore.YELLOW}[x]missing or incorrect arguments")
 
